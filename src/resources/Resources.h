6fe9596a5473318855e84a56d992f1aead9b44b0
piegit <pietrainlover21@gmail.com>
2022-01-28 13:51:43 -0700

    Update Resources.h

--- src/resources/Resources.h
+++ src/resources/Resources.h
@@ -5,9 +5,6 @@
 #include "common/types.h"
 
 //! forward declaration
-class GuiImageData;
-class GuiSound;
-
 class Resources
 {
 public:
@@ -15,20 +12,11 @@ public:
     static bool LoadFiles(const char * path);
     static const u8 * GetFile(const char * filename);
     static u32 GetFileSize(const char * filename);
-
-    static GuiImageData * GetImageData(const char * filename);
-    static void RemoveImageData(GuiImageData * image);
-
-    static GuiSound * GetSound(const char * filename);
-    static void RemoveSound(GuiSound * sound);
 private:
     static Resources *instance;
 
     Resources() {}
     ~Resources() {}
-
-    std::map<std::string, std::pair<unsigned int, GuiImageData *> > imageDataMap;
-    std::map<std::string, std::pair<unsigned int, GuiSound *> > soundDataMap;
 };
 
 #endif
